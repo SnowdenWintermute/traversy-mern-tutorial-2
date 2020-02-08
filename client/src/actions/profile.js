2f89f30d71a4c5d42333a39f5fbd21634f11e0d0
unknown <michael.p.silverman@gmail.com>
2020-02-08 16:39:06 -0500

    edit profile

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { setAlert } from "./alert";
 
-import { GET_PROFILE, GET_PROFILES, PROFILE_ERROR } from "./types";
+import { GET_PROFILE, GET_PROFILES, PROFILE_ERROR, SET_ALERT } from "./types";
 
 // get current user profile
 export const getCurrentProfile = () => async dispatch => {
@@ -12,6 +12,44 @@ export const getCurrentProfile = () => async dispatch => {
       payload: res.data
     });
   } catch (error) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: error.response.data.msg
+          ? error.response.data.msg
+          : error.responseText,
+        status: error.response.status
+      }
+    });
+  }
+};
+
+// create or update a profile
+export const createProfile = (
+  formData,
+  history,
+  edit = false
+) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+    const res = await axios.post("/api/profile/", formData, config);
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+    dispatch(setAlert(edit ? "Profile updated" : "Profile created", success));
+    if (!edit) {
+      history.push("/dashboard");
+    }
+  } catch (error) {
+    const errors = error.response.data.errors;
+    if (errors) {
+      errors.forEach(err => dispatch(setAlert(err.msg, "danger")));
+    }
     dispatch({
       type: PROFILE_ERROR,
       payload: {
